be45b7a8a338eb065b8154213830691ffe26ee99
thomas <1902916022@qq.com>
2021-10-20 23:42:49 +0800

    a test

---
+++ src/main/java/cn/thomas/collectepidemic/CollectEpidemicApplication.java
@@ -0,0 +1,14 @@
+package cn.thomas.collectepidemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectEpidemicApplication {
+
+    public static void main(String[] args) {
+        System.out.println("this is test");
+        SpringApplication.run(CollectEpidemicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/thomas/collectepidemic/CollectEpidemicApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.thomas.collectepidemic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectEpidemicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
